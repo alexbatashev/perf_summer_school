de8790d94b3c1422865e03b9be55b68f83201c90
Alexander Batashev <alexbatashev@outlook.com>
2024-06-16 11:37:47 +0300

    add missing include

--- tests/task2.cpp
+++ tests/task2.cpp
@@ -3,6 +3,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/matchers/catch_matchers_floating_point.hpp>
 
+#include <array>
+
 inline constexpr int TEST_SIZE = 100;
 
 using Catch::Matchers::WithinRel;
